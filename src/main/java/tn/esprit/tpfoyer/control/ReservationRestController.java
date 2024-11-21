ab41b1f105c9fbcee665b4a8ec56d9a5f236ebcf
slimfarhat123 <slim.farhat@esprit.tn>
2024-11-21 11:03:13 +0100

    update

--- src/main/java/tn/esprit/tpfoyer/control/ReservationRestController.java
+++ src/main/java/tn/esprit/tpfoyer/control/ReservationRestController.java
@@ -19,14 +19,12 @@ public class ReservationRestController {
     // http://localhost:8089/tpfoyer/reservation/retrieve-all-reservations
     @GetMapping("/retrieve-all-reservations")
     public List<Reservation> getReservations() {
-        List<Reservation> listReservations = reservationService.retrieveAllReservations();
-        return listReservations;
+        return reservationService.retrieveAllReservations();
     }
     // http://localhost:8089/tpfoyer/reservation/retrieve-reservation/8
     @GetMapping("/retrieve-reservation/{reservation-id}")
     public Reservation retrieveReservation(@PathVariable("reservation-id") String rId) {
-        Reservation reservation = reservationService.retrieveReservation(rId);
-        return reservation;
+        return reservationService.retrieveReservation(rId);
     }
 
 
@@ -54,8 +52,7 @@ public class ReservationRestController {
     // http://localhost:8089/tpfoyer/reservation/add-reservation
     @PostMapping("/add-reservation")
     public Reservation addReservation(@RequestBody Reservation r) {
-        Reservation reservation = reservationService.addReservation(r);
-        return reservation;
+        return reservationService.addReservation(r);
     }
 
     @DeleteMapping("/remove-reservation/{reservation-id}")
@@ -66,8 +63,7 @@ public class ReservationRestController {
     // http://localhost:8089/tpfoyer/reservation/modify-reservation
     @PutMapping("/modify-reservation")
     public Reservation modifyReservation(@RequestBody Reservation r) {
-        Reservation reservation = reservationService.modifyReservation(r);
-        return reservation;
+        return reservationService.modifyReservation(r);
     }
 
 }
