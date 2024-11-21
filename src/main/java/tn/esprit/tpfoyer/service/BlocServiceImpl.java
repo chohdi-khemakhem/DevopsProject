721d3040286079b4fd47253c30292132deb93260
slimfarhat123 <slim.farhat@esprit.tn>
2024-11-21 16:36:37 +0100

    update

--- src/main/java/tn/esprit/tpfoyer/service/BlocServiceImpl.java
+++ src/main/java/tn/esprit/tpfoyer/service/BlocServiceImpl.java
@@ -51,8 +51,7 @@ public class BlocServiceImpl  implements IBlocService {
     @Transactional
     public Bloc retrieveBloc(Long blocId) {
 
-        return blocRepository.findById(blocId).get();
-    }
+        return blocRepository.findById(blocId).get();    }
 
 
     public Bloc addBloc(Bloc c) {
