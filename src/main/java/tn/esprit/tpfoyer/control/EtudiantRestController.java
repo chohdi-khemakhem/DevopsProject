9fce45828349d2ead3c4aac8c7f96b5ecae6b020
aissa swiden <aissa.swiden@infinitymgt.fr>
2024-11-21 11:22:28 +0100

    angular test

--- src/main/java/tn/esprit/tpfoyer/control/EtudiantRestController.java
+++ src/main/java/tn/esprit/tpfoyer/control/EtudiantRestController.java
@@ -11,6 +11,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/etudiant")
+@CrossOrigin("*")
 public class EtudiantRestController {
 
     IEtudiantService etudiantService;
