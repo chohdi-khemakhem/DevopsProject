f5e7a20e72eb1c76d3cd3103865573364f21dc0b
aissa swiden <aissa.swiden@infinitymgt.fr>
2024-11-21 16:04:23 +0100

    angular crossorigin

--- src/main/java/tn/esprit/tpfoyer/control/EtudiantRestController.java
+++ src/main/java/tn/esprit/tpfoyer/control/EtudiantRestController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/etudiant")
-@CrossOrigin("*")
+@CrossOrigin("http://192.168.50.4")
 public class EtudiantRestController {
 
     IEtudiantService etudiantService;
