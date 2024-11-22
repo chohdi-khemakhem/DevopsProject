bda1fa630e780ef934113482a62a9c7102fb5102
Ahmed-Naifer <ahmednaifer.galaxus@gmail.com>
2024-11-22 01:58:36 +0100

    update17

--- src/main/java/tn/esprit/tpfoyer/TpFoyerApplication.java
+++ src/main/java/tn/esprit/tpfoyer/TpFoyerApplication.java
@@ -1,11 +1,15 @@
 package tn.esprit.tpfoyer;
 
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import tn.esprit.tpfoyer.entity.Universite;
+import tn.esprit.tpfoyer.repository.UniversiteRepository;
 
 
 @SpringBootApplication
@@ -16,5 +20,21 @@ public class TpFoyerApplication {
     public static void main(String[] args) {
         SpringApplication.run(TpFoyerApplication.class, args);
     }
+    @Bean
+    CommandLineRunner runner(UniversiteRepository universiteRepository) {
+        return args -> {
 
+            Universite esprit = new Universite();
+            esprit.setNomUniversite("esprit");
+            esprit.setAdresse("ariana");
+
+
+
+            universiteRepository.save(esprit);
+
+
+
+
+        };
+    }
 }
